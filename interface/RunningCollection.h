8e3f25ba56bed90f9838582f2f8397c12f7306f7
ywkao <ywkao@hep1.phys.ntu.edu.tw>
2023-02-21 15:31:48 +0100

    Fix multiple definition issue

--- interface/RunningCollection.h
+++ interface/RunningCollection.h
@@ -3,44 +3,31 @@
 #include <vector>
 #include "RunningStatistics.h"
 
-class RunningCollection{
-    public:
-        RunningCollection();
-        ~RunningCollection();
+class RunningCollection {
+public:
+    RunningCollection() {
+        Nchannels = 234;
+        for(int i=0; i<Nchannels; ++i) {
+            RunningStatistics instance;
+            mVecRunningStatistics.push_back(instance);
+        }
+    }
 
-        void add_entry(int channleId, double value, double cm);
-        std::vector<RunningStatistics> get_vector_running_statistics() {return mVecRunningStatistics;};
-        void write_csv_file();
+    void add_entry(int channleId, double value, double cm) {
+        if(channleId>=234) {
+            printf("[ERROR] RunningCollection::add_entry: the channel ID is out of scope\n");
+            return;
+        }
+        mVecRunningStatistics[channleId].add_entry(cm, value);
+    }
 
-    private:
-        int Nchannels;
-        std::vector<RunningStatistics> mVecRunningStatistics;
+    std::vector<RunningStatistics> get_vector_running_statistics() {return mVecRunningStatistics;};
 
+    void write_csv_file() {};
+
+private:
+    int Nchannels;
+    std::vector<RunningStatistics> mVecRunningStatistics;
 };
-
-RunningCollection::RunningCollection()
-{
-    Nchannels = 234;
-
-    for(int i=0; i<Nchannels; ++i)
-    {
-        RunningStatistics instance;
-        mVecRunningStatistics.push_back(instance);
-    }
-}
-
-RunningCollection::~RunningCollection() {}
-
-void RunningCollection::add_entry(int channleId, double value, double cm)
-{
-    if(channleId>=234) {
-        printf("[ERROR] RunningCollection::add_entry: the channel ID is out of scope\n");
-        return;
-    }
-
-    mVecRunningStatistics[channleId].add_entry(cm, value);
-}
-
-void RunningCollection::write_csv_file() {};
 
 #endif
