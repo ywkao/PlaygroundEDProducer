e0403edc6aee7d667ac8eac6d7156dd6a8dba139
ywkao <ywkao@hep1.phys.ntu.edu.tw>
2023-02-21 15:56:06 +0100

    Use EmptySource

--- python/ExampleConfig_cfg.py
+++ python/ExampleConfig_cfg.py
@@ -4,15 +4,8 @@ process = cms.Process("Demo")
 
 process.load("FWCore.MessageService.MessageLogger_cfi")
 
-# process one event temporarily (we loop over an ntuple in analyze()...)
-process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(1) )
-
-process.source = cms.Source("PoolSource",
-    # A dummy step2.root file
-    fileNames = cms.untracked.vstring(
-        'file:/eos/user/y/ykao/www/HGCAL_Geant4_project/testbeam_positron_D86_R80To130_E100/step2.root'
-    )
-)
+process.maxEvents.input = 1
+process.source = cms.Source('EmptySource')
 
 #--------------------------------------------------
 # EDProducer
